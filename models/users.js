6b102228a562fbc0d8b477c4c9d9a23bfcaf73e8
Foucsi <julien.foucartreact@gmail.com>
2023-01-02 20:01:32 +0100

    commit

--- models/users.js
+++ models/users.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
+const todoSchema = mongoose.Schema({
+  todo: String,
+});
+
 const userSchema = mongoose.Schema({
   username: String,
   email: String,
   password: String,
   token: String,
+  todo: [todoSchema],
 });
 
 const User = mongoose.model("users", userSchema);
