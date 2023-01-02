97ea887cd7b19fdc237333ccb5f69a6d46b0c51e
Foucsi <julien.foucartreact@gmail.com>
2023-01-02 18:54:48 +0100

    finish signi/signup

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,47 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
+/* post a new user */
+
+router.post("/signup", (req, res) => {
+  if (!checkBody(req.body, ["username", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  User.findOne({ username: req.body.username }).then((data) => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hash,
+        token: uid2(32),
+        tweets: [],
+      });
+
+      newUser.save().then((data) => {
+        res.json({ result: true, user: data });
+      });
+    } else {
+      res.json({ result: false, error: "User already exists" });
+    }
+  });
+});
+
+router.post("/signin", (req, res) => {
+  if (!checkBody(req.body, ["username", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+  User.findOne({ username: req.body.username }).then((data) => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, user: data });
+    } else {
+      res.json({ result: false, error: "User not found or wrong password" });
+    }
+  });
+});
+
 module.exports = router;
