1daceb67e1a6131db3f3faa384929d7ac4ae051d
Foucsi <julien.foucartreact@gmail.com>
2023-01-02 15:14:10 +0100

    first commit back

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+require("../models/connection");
+const { checkBody } = require("../modules/checkBody");
+const uid2 = require("uid2");
+const bcrypt = require("bcrypt");
+
+const User = require("../models/users");
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+module.exports = router;
